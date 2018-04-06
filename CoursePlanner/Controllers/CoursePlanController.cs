9142304672997596e9510842057b432eab62ce40
eal61 <eal61@pitt.edu>
2018-04-06 14:00:44 -0400

    work on ajax call

---
+++ CoursePlanner/Controllers/CoursePlanController.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+using CoursePlanner.Models;
+using System.Web.Http;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace CoursePlanner.Controllers
+{
+    public class CoursePlanController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var name = User.Identity.Name;
+            return Ok(name);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post()
+        {
+            return Ok();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            string connectionString = "Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\aspnet-CoursePlanner-20180131110323.mdf;Initial Catalog=aspnet-CoursePlanner-20180131110323;Integrated Security=True";
+            int success;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("", conn);
+                command.Parameters.Add(new SqlParameter());
+                command.Parameters.Add(new SqlParameter());
+                success = command.ExecuteNonQuery();
+                conn.Close();
+            }
+            return Ok(success > 0);
+        }
+    }
+}
--- CoursePlanner/Models/CoursePlan.cs
+++ CoursePlanner/Models/CoursePlan.cs
@@ -9,4 +9,16 @@ namespace CoursePlanner.Models
     {
         public List<Semester> Semesters { get; set; }   // this should allow us to accomodate more than 4 yr plans
     }
+
+    public class ClientCoursePlan
+    {
+        public List<ClientCourseInfo> plan { get; set; }
+    }
+
+    public class ClientCourseInfo
+    {
+        public int semesterId { get; set; }
+        public int courseId { get; set; }
+    }
+
 }
